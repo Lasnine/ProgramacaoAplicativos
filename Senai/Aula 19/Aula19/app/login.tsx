5907c0f3b9a4e332fef90b218ff4ce9d6ebfc053
Lasnine <lasninemiranda@gmail.com>
2025-11-12 11:43:09 -0300

    Aula 19

---
+++ Senai/Aula 19/Aula19/app/login.tsx
@@ -0,0 +1,77 @@
+import { Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+export default function Login() {
+  return (
+    <>  
+        <View style={styles.alinhamento}>
+          <View style={styles.square}></View>
+          <View style={styles.margem}>
+            <Text style={[styles.Color,  { marginTop: 60 }]}>LOGIN</Text>
+            <TextInput style={[styles.input,  { marginTop: 15 }]} placeholder='Usuario'/>
+            <TextInput style={[styles.input,  { marginTop: 15 }]} secureTextEntry={true} placeholder='Senha'/>
+            <TouchableOpacity style={[styles.button, { marginTop: 15 }]}>
+              <View>
+                <Text style={[styles.text, { marginTop: 10 }]}>
+                  LOGIN
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.square2}></View>
+        </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  square:{
+    width: '100%',
+    height: '2%',
+    backgroundColor: 'black',
+  },
+  square2:{
+    
+    width: '100%',
+    height: '2%',
+    backgroundColor: 'black',
+  },
+  margem:{
+    padding: 30,
+  },
+  Color:{
+    color: 'black',
+    fontFamily: 'Heather',
+    fontWeight: 'bold',
+    fontSize: 50,
+    textAlign: 'center',
+  },
+  alinhamento:{
+    justifyContent: 'space-between',
+    height: '100%',
+    textAlign: 'center',
+    alignSelf: 'center',
+    width: '100%',
+ 
+  },
+  input: {
+    backgroundColor: 'white',
+    borderColor: 'black',  
+    borderWidth: 1,
+    padding: 10, 
+    borderRadius: 10,
+  },
+  text:{
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 15,
+    textAlign: 'center',
+    alignSelf: 'center',
+  },
+  button:{
+    height: 40,
+    width: 100,
+    backgroundColor: '#DEDEDE',
+    borderRadius: 10,
+    alignSelf: 'center',
+  },
+});
